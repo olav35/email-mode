918cc07bb6948ebe4246a5ef33d7483664cbbb35
Olav Fosse <mail@olavfosse.no>
2020-11-02 22:10:07 +0100

    Make boilerplate package / major-mode

---
+++ email-mode.el
@@ -0,0 +1,31 @@
+;;; email-mode.el --- An email composition mode                       -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Olav Fosse
+
+;; Author: Olav Fosse <mail@olavfosse.no>
+;; Version: 0.1
+;; URL: https://github.com/olav35/email-mode
+;; Package-Requires: ((emacs "24.1"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; An email composition mode
+
+;;; Code:
+
+(define-derived-mode email-mode text-mode "Email")
+
+;;; email-mode.el ends here
